0f57313ef3a9f5269b0acd596c6d1523620184d9
marius <mariusmillea@gmail.com>
2022-01-12 00:35:48 -0800

    make Jax JIT optional

--- muse_inference/jax.py
+++ muse_inference/jax.py
@@ -15,23 +15,38 @@ class JaxMuseProblem(MuseProblem):
     def logPrior(self, θ):
         raise NotImplementedError()
 
-    @partial(jax.jit, static_argnums=(0,))
     def gradθ_logLike(self, x, z, θ):
         return jax.grad(lambda θ: self.logLike(x, z, θ))(θ)
 
-    @partial(jax.jit, static_argnums=(0,))
     def logLike_and_gradz_logLike(self, x, z, θ):
         logLike = self.logLike(x, z, θ)
         gradz_logLike = jax.grad(lambda z: self.logLike(x, z, θ))(z)
         return (logLike, gradz_logLike)
 
-    @partial(jax.jit, static_argnums=(0,))
     def zMAP_at_θ(self, x, z0, θ, gradz_logLike_atol=None):
         soln = minimize(lambda z: -self.logLike(x, z, θ), z0, method="BFGS", tol=gradz_logLike_atol)
         return (soln.x, soln)
 
-    @partial(jax.jit, static_argnums=(0,))
     def grad_hess_θ_logPrior(self, θ):
         g = jax.grad(self.logPrior)(θ)
         H = jax.hessian(self.logPrior)(θ)
         return (g,H)
+
+
+class JittedJaxMuseProblem(JaxMuseProblem):
+
+    @partial(jax.jit, static_argnums=(0,))
+    def gradθ_logLike(self, x, z, θ):
+        return super().gradθ_logLike(x, z, θ)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def logLike_and_gradz_logLike(self, x, z, θ):
+        return super().logLike_and_gradz_logLike(x, z, θ)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def zMAP_at_θ(self, x, z0, θ, gradz_logLike_atol=None):
+        return super().zMAP_at_θ(x, z0, θ, gradz_logLike_atol)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def grad_hess_θ_logPrior(self, θ):
+        return super().grad_hess_θ_logPrior(θ)
